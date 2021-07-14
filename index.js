6ab2271088fad3c8fecfcf6313ad729b0f363465
RAYMOND TAN <61599941+rtan0624@users.noreply.github.com>
2021-07-14 22:23:28 +0000

    database

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const { prefix } = require('./config.json');
 const { Collection } = require("discord.js");
 const voiceCollection = new Collection();
 const command = require('./command.js');
+const Database = require("@replit/database")
 
 const mySecret = process.env['TOKEN']
+const db = new Database()
+
 
 client.on('ready', () => {
   console.log('Client is ready')
@@ -25,17 +28,39 @@ client.on('ready', () => {
       console.log(channel)
     })
   })
+  command(client, ['setup', 'start'], (message) => {
+    message.guild.channels.create("=>||Private Channels||<=", {
+      type: "category",
+    }).then((category) => {
+      const categoryID = category.id
+      console.log(categoryID)
+       // << try now?
+      message.guild.channels.create("-Private Channels-", {
+      type: "voice",
+      parent: categoryID
+      }).then((dbChannel) => {
+      const dbChannelID = dbChannel.id
+      const guildID = "" + message.guild.id
+      console.log(dbChannelID)
+      db.set(guildID, [categoryID, dbChannelID])
+      db.get(guildID, { raw: false }).then(console.log);
+  })
 })
+  })})
 
 client.on('voiceStateUpdate', async (oldState,newState) => {
   const user = await client.users.fetch(newState.id);
   const member = newState.guild.member(user);
   const userValue = user.id
-
-  if(!oldState.channel && newState.channel.id === '864602195126124556'){
+  const guildID = "" + member.voice.guild.id
+  let dbArray = await db.get(guildID);
+  let dBChannelID = "" + dbArray[1]
+  let dBCategoryID = "" + dbArray[0]
+  console.log(dBChannelID)
+  if(!oldState.channel && newState.channel.id === dBChannelID){
     const channel = await newState.guild.channels.create(user.tag, {
       type: 'voice',
-      parent: "864654875442806794",
+      parent: dBCategoryID,
       permissionOverwrites: [
         {
 			id: newState.guild.id,
@@ -56,5 +81,6 @@ client.on('voiceStateUpdate', async (oldState,newState) => {
     if(oldState.channelID === voiceCollection.get(newState.id)) return oldState.channel.delete()
   }
 })
+
 
 client.login(mySecret)
